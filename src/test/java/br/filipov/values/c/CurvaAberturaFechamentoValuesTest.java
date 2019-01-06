1aa1f9cc55e9e1a606cd6e252f0e22a36da6f463
Marcelo A. Filipov <marcelo.filipov@gmail.com>
2019-01-06 14:57:22 -0200

    updated ajust sonar

--- src/test/java/br/filipov/values/c/CurvaAberturaFechamentoValuesTest.java
+++ src/test/java/br/filipov/values/c/CurvaAberturaFechamentoValuesTest.java
@@ -1,6 +1,7 @@
 package br.filipov.values.c;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,37 +20,45 @@ public class CurvaAberturaFechamentoValuesTest {
 		assertEquals(CurvaAberturaFechamentoValues.ABERTA, EnumHelper.getEnumPorCodigo(1, CurvaAberturaFechamentoValues.class));
 		assertEquals(CurvaAberturaFechamentoValues.FECHADA, EnumHelper.getEnumPorCodigo(2, CurvaAberturaFechamentoValues.class));
 	}	
-
-	
 	
 	@Test
 	public void testGetDescricaoByIdShort() {
-		assertEquals("Abertura",CurvaAberturaFechamentoValues.getDescricaoById((short) 1));
-		assertEquals("Fechamento",CurvaAberturaFechamentoValues.getDescricaoById((short) 2));
+		assertEquals("Abertura", CurvaAberturaFechamentoValues.getDescricaoById((short) 1));
+		assertEquals("Fechamento", CurvaAberturaFechamentoValues.getDescricaoById((short) 2));
+		
+		assertNotEquals("N/A", CurvaAberturaFechamentoValues.getDescricaoById((short) 9));
 	}
 
 	@Test
 	public void testGetDescricaoByIdInteger() {
-		assertEquals("Abertura",CurvaAberturaFechamentoValues.getDescricaoById(1));
-		assertEquals("Fechamento",CurvaAberturaFechamentoValues.getDescricaoById(2));
+		assertEquals("Abertura", CurvaAberturaFechamentoValues.getDescricaoById(1));
+		assertEquals("Fechamento", CurvaAberturaFechamentoValues.getDescricaoById(2));
+		
+		assertNotEquals("N/A", CurvaAberturaFechamentoValues.getDescricaoById(9));
 	}
 
 	@Test
 	public void testGetDescricaoByIdLong() {
-		assertEquals("Abertura",CurvaAberturaFechamentoValues.getDescricaoById(1L));
-		assertEquals("Fechamento",CurvaAberturaFechamentoValues.getDescricaoById(2L));
+		assertEquals("Abertura", CurvaAberturaFechamentoValues.getDescricaoById(1L));
+		assertEquals("Fechamento", CurvaAberturaFechamentoValues.getDescricaoById(2L));
+		
+		assertNotEquals("N/A", CurvaAberturaFechamentoValues.getDescricaoById(9L));
 	}	
 	
 	@Test
 	public void testGetDescricaoByIdByte() {
-		assertEquals("Abertura",CurvaAberturaFechamentoValues.getDescricaoById((byte) 1));
-		assertEquals("Fechamento",CurvaAberturaFechamentoValues.getDescricaoById((byte) 2));
+		assertEquals("Abertura", CurvaAberturaFechamentoValues.getDescricaoById((byte) 1));
+		assertEquals("Fechamento", CurvaAberturaFechamentoValues.getDescricaoById((byte) 2));
+		
+		assertNotEquals("N/A", CurvaAberturaFechamentoValues.getDescricaoById((byte) 9));
 	}
 
 	@Test
 	public void testParse() {
-		assertEquals(CurvaAberturaFechamentoValues.ABERTA,CurvaAberturaFechamentoValues.parse(1));
-		assertEquals(CurvaAberturaFechamentoValues.FECHADA,CurvaAberturaFechamentoValues.parse(2));
+		assertEquals(CurvaAberturaFechamentoValues.ABERTA, CurvaAberturaFechamentoValues.parse(1));
+		assertEquals(CurvaAberturaFechamentoValues.FECHADA, CurvaAberturaFechamentoValues.parse(2));
+		
+		assertNotEquals(CurvaAberturaFechamentoValues.FECHADA, CurvaAberturaFechamentoValues.parse(9));
 	}	
 	
 }
--- src/test/java/br/filipov/values/d/DCDUValuesTest.java
+++ src/test/java/br/filipov/values/d/DCDUValuesTest.java
@@ -1,6 +1,7 @@
 package br.filipov.values.d;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,42 +22,50 @@ public class DCDUValuesTest {
 		assertEquals(DCDUValues.DC, EnumHelper.getEnumPorCodigo(1, DCDUValues.class));
 		assertEquals(DCDUValues.DU, EnumHelper.getEnumPorCodigo(2, DCDUValues.class));
 	}	
-
-	
 	
 	@Test
 	public void testGetDescricaoByIdShort() {
-		assertEquals("Todos",DCDUValues.getDescricaoById((short) 0));
-		assertEquals("DC",DCDUValues.getDescricaoById((short) 1));
-		assertEquals("DU",DCDUValues.getDescricaoById((short) 2));
+		assertEquals("Todos", DCDUValues.getDescricaoById((short) 0));
+		assertEquals("DC", DCDUValues.getDescricaoById((short) 1));
+		assertEquals("DU", DCDUValues.getDescricaoById((short) 2));
+		
+		assertNotEquals("N/A", DCDUValues.getDescricaoById((short) 9));
 	}
 
 	@Test
 	public void testGetDescricaoByIdInteger() {
-		assertEquals("Todos",DCDUValues.getDescricaoById(0));
-		assertEquals("DC",DCDUValues.getDescricaoById(1));
-		assertEquals("DU",DCDUValues.getDescricaoById(2));
+		assertEquals("Todos", DCDUValues.getDescricaoById(0));
+		assertEquals("DC", DCDUValues.getDescricaoById(1));
+		assertEquals("DU", DCDUValues.getDescricaoById(2));
+		
+		assertNotEquals("N/A", DCDUValues.getDescricaoById(9));
 	}
 
 	@Test
 	public void testGetDescricaoByIdLong() {
-		assertEquals("Todos",DCDUValues.getDescricaoById(0L));
-		assertEquals("DC",DCDUValues.getDescricaoById(1L));
-		assertEquals("DU",DCDUValues.getDescricaoById(2L));
+		assertEquals("Todos", DCDUValues.getDescricaoById(0L));
+		assertEquals("DC", DCDUValues.getDescricaoById(1L));
+		assertEquals("DU", DCDUValues.getDescricaoById(2L));
+		
+		assertNotEquals("N/A", DCDUValues.getDescricaoById(9L));
 	}	
 	
 	@Test
 	public void testGetDescricaoByIdByte() {
-		assertEquals("Todos",DCDUValues.getDescricaoById((byte) 0));
-		assertEquals("DC",DCDUValues.getDescricaoById((byte) 1));
-		assertEquals("DU",DCDUValues.getDescricaoById((byte) 2));
+		assertEquals("Todos", DCDUValues.getDescricaoById((byte) 0));
+		assertEquals("DC", DCDUValues.getDescricaoById((byte) 1));
+		assertEquals("DU", DCDUValues.getDescricaoById((byte) 2));
+		
+		assertNotEquals("N/A", DCDUValues.getDescricaoById((byte) 9));
 	}
 
 	@Test
 	public void testParse() {
-		assertEquals(DCDUValues.TODOS,DCDUValues.parse(0));
-		assertEquals(DCDUValues.DC,DCDUValues.parse(1));
-		assertEquals(DCDUValues.DU,DCDUValues.parse(2));
+		assertEquals(DCDUValues.TODOS, DCDUValues.parse(0));
+		assertEquals(DCDUValues.DC, DCDUValues.parse(1));
+		assertEquals(DCDUValues.DU, DCDUValues.parse(2));
+		
+		assertNotEquals(DCDUValues.DU, DCDUValues.parse(9));
 	}	
 
 }
