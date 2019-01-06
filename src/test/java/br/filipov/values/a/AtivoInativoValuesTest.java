1c84c6a5f5a5480602ee073bb4e4dbb35276ab30
Marcelo A. Filipov <marcelo.filipov@gmail.com>
2019-01-06 14:19:55 -0200

    updated ajust sonar

--- src/test/java/br/filipov/values/a/AtivoInativoValuesTest.java
+++ src/test/java/br/filipov/values/a/AtivoInativoValuesTest.java
@@ -1,6 +1,7 @@
 package br.filipov.values.a;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,32 +23,42 @@ public class AtivoInativoValuesTest {
 	
 	@Test
 	public void testGetDescricaoByIdShort() {
-		assertEquals("Ativo",AtivoInativoValues.getDescricaoById((short) 1));
-		assertEquals("Inativo",AtivoInativoValues.getDescricaoById((short) 0));
+		assertEquals("Ativo", AtivoInativoValues.getDescricaoById((short) 1));
+		assertEquals("Inativo", AtivoInativoValues.getDescricaoById((short) 0));
+		
+		assertNotEquals("N/A", AtivoInativoValues.getDescricaoById((short) 9));
 	}
 
 	@Test
 	public void testGetDescricaoByIdInteger() {
-		assertEquals("Ativo",AtivoInativoValues.getDescricaoById(1));
-		assertEquals("Inativo",AtivoInativoValues.getDescricaoById(0));
+		assertEquals("Ativo", AtivoInativoValues.getDescricaoById(1));
+		assertEquals("Inativo", AtivoInativoValues.getDescricaoById(0));
+		
+		assertNotEquals("N/A", AtivoInativoValues.getDescricaoById(9));
 	}
 
 	@Test
 	public void testGetDescricaoByIdLong() {
-		assertEquals("Ativo",AtivoInativoValues.getDescricaoById(1L));
-		assertEquals("Inativo",AtivoInativoValues.getDescricaoById(0L));
+		assertEquals("Ativo", AtivoInativoValues.getDescricaoById(1L));
+		assertEquals("Inativo", AtivoInativoValues.getDescricaoById(0L));
+		
+		assertNotEquals("N/A", AtivoInativoValues.getDescricaoById(9L));
 	}	
 	
 	@Test
 	public void testGetDescricaoByIdByte() {
-		assertEquals("Ativo",AtivoInativoValues.getDescricaoById((byte) 1));
-		assertEquals("Inativo",AtivoInativoValues.getDescricaoById((byte) 0));
+		assertEquals("Ativo", AtivoInativoValues.getDescricaoById((byte) 1));
+		assertEquals("Inativo", AtivoInativoValues.getDescricaoById((byte) 0));
+		
+		assertNotEquals("N/A", AtivoInativoValues.getDescricaoById((byte) 9));
 	}
 
 	@Test
 	public void testParse() {
 		assertEquals(AtivoInativoValues.ATIVO,AtivoInativoValues.parse(1));
 		assertEquals(AtivoInativoValues.INATIVO,AtivoInativoValues.parse(0));
+		
+		assertNotEquals(AtivoInativoValues.INATIVO,AtivoInativoValues.parse(9));
 	}	
 	
 }
